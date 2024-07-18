f13db9a79620ec077dce029dd48aac69ad482b9f
frogfrequency <66001719+frogfrequency@users.noreply.github.com>
2024-07-18 17:22:01 +0200

    Add more english words

--- textJS.js
+++ textJS.js
@@ -98,10 +98,119 @@ to contemplate sth. etwas andenken
 contention die Auseinandersetzung
 contention die Behauptung
 contentious streitsüchtig`,
+// #################################
 "english_2": `asdf
 asdf
 asdf
 asds
 f
-asdf`
+asdf`,
+// #################################
+"english_5": `
+reverence (for someone/something) die Ehrfurcht (vor jemandem/etwas)
+rigorous gründlich
+riveting fesselnd
+to rouse aufscheuchen
+ruse die List
+rusk der Zwieback
+sacrilege die Lästerung/der Frevel
+salient vorspringend/auffallend
+savvy gerissen
+savvy klug
+scourge die Plage/ die Peitsche
+to scry aus einer Kristallkugel wahrsagen/erspähen
+self-insured eigenversichert
+to sequester sich abkapseln
+serendipity glücklicher Zufall
+serenity die Gelassenheit
+shambolic chaotisch
+shifty verschlagen
+shill der Lockvogel
+short shrift die Galgenfrist
+shrift die Beichte
+shrewd scharfsinnig
+to shun someone/something jemanden/etwas meiden
+slander die Verunglimpfung
+smitten befallen
+smug selbstgefällig
+snag der Baumstumpf/der Stummel
+snappy flott/zackig
+solace der Trost
+soot der Russ
+sorely schmerzlich
+spall der Splitter
+spoof die Parodie
+spoof der Schwindel
+to spruce someone up jemanden herausputzen
+to spur anregen/aufstacheln
+spurious unecht
+to spurn someone/something jemanden/etwas zurückweisen
+to squander something etwas vergeuden
+to stage-manage inszenieren
+stark kahl
+stark sachlich
+stationery die Schreibware
+to staunch etwas hemmen
+staunch überzeugt
+steadfast standhaft
+stern das Hinterteil/Heck
+stingy geizig
+stockade die Palisade
+to stoke etwas schüren/anfachen
+stunted verkrüppelt
+sty der Schweinestall/das Gerstenkorn
+sublime erhaben
+subpar unterdurchschnittlich
+sultry schwül/drückend/sinnlich
+superfluous überflüssig/gegenstandslos
+supple geschmeidig
+surety die Bürgschaft/Sicherheit/Kaution
+surrogate der Stellvertreter
+sustenance der Unterhalt
+synopsis die Übersicht/Zuammenfassung
+tacky klebrig/billig/schäbig
+tainted unrein/befleckt
+tally der Zähler
+tantalizing verlockend/quälend/spannend
+tantamount gleichbedeutend
+tapering das Reduzieren/Ausdünnen/Verjüngen
+to tarnish matt werden/anlaufen/trüben
+temerity die Kühnheit/Frechheit/Unverfrohrenheit
+tenacity die Beharrlichkeit
+tenure der Besitz/die Beschäftigungsdauer
+thimble der Fingerhut
+tidbit der Gabelbissen/Leckerbissen
+to toil schuften/quälen/plagen
+to tout something etwas aufdringlich ankündigen
+transient vorübergehend
+trepidation die Ängstlichkeit
+tripartite dreigliedrig
+to trounce scharf zurechtweisen/verurteilen/verhauen
+to trudge stampfen/trotten
+ulcer das Geschwür
+unequivocal unmissverständlich
+unfettered ungehindert
+unperturbed unbeeindruckt
+unsolicited unverlangt
+untenable haltlos/unhaltbar
+to unwind sich entspannen/loswickeln/abspulen
+to upend something etwas kippen
+upheaval der Umbruch/die Erhebung
+vanguard der Vorreiter/Vortrupp
+vertigo die Höhenangst/der Schwindel
+to vest übertragen/jemanden bevollmächtigen/einkleiden
+to vet jemanden/etwas überprüfen
+to vex someone jemanden ärgern
+to vindicate someone/something jemanden rehabilitieren/etwas rechtfertigen/verteidigen
+vindictiveness die Rachsucht
+voracious unersättlich/gierig/gefrässig
+to wane abnehmen/schwinden
+warring kriegführend
+wary vorsichtig
+wharf der Anlegeplatz/Kai
+whim die Laune
+yonder jener/jene/jenes ... dort (drüben)
+zealous eifrig
+zesty begeistert/pikant
+`
 }
